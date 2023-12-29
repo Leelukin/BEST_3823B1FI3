47f2dce33ade952664e5b8a316c2064e16c3553b
Leelukin <lukinilua@gmail.com>
2023-12-29 12:22:12 +0300

    -098

--- 6-functions/one_array/generator.h
+++ 6-functions/one_array/generator.h
@@ -1,5 +1,21 @@
 #include <math.h>
+#include <stdlib.h>
 
 int* array_generator(int* real_size, int k, int m, int f){
-    return 0;
+    *real_size = 30;
+    int i = 0;
+    int* array = (int*)calloc(sizeof(int), *real_size);
+    array[*real_size - 1] = f;
+    int summa = f;
+    while(summa < m){
+        if(summa < k){
+            if(array[i]< f){
+                i++;
+                summa++;
+            }
+        }
+        if (summa > k)
+        break;
+    }
+    return array;
 }
