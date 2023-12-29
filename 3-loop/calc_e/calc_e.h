176babe8e57ddd5c416501007b0273712b8c11d1
Leelukin <lukinilua@gmail.com>
2023-12-29 12:13:51 +0300

    nm

--- 3-loop/calc_e/calc_e.h
+++ 3-loop/calc_e/calc_e.h
@@ -1,5 +1,14 @@
 #include <math.h>
 
 unsigned long long calc_e(char order){
-    return 0;
+    double i;
+    int t = floor(exp(1) * pow(10, order));
+    int flag = 0;
+    for (i = 1; flag == 0; i++)
+    {
+        double y = pow((1 + 1 / i), i);
+        if(floor(y * pow(10, order)) == t)
+        flag = 1;
+    }
+    return (unsigned long long) i;
 }
